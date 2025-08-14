c58b6b61fb3421af189bcc0994bb725b3a417bd7
AdarshaCoding <adarshapc4467@gmail.com>
2025-08-14 23:21:07 +0530

    refactor(db): move MongoDB URI to environment variable

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
-
-const CONNECTION_STRING =
-  "mongodb+srv://adarsha-node_01:adarsha123@cluster-1.awqvyfp.mongodb.net/devTinder";
-
+require("dotenv").config();
 const connectDB = async () => {
-  await mongoose.connect(CONNECTION_STRING);
+  await mongoose.connect(process.env.CONNECTION_STRING);
 };
 
 module.exports = connectDB;
