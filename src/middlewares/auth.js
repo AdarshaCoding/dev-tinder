b56669900892aba8b3e77520e901dcb1e46a393a
AdarshaCoding <adarshapc4467@gmail.com>
2025-08-20 08:48:32 +0530

    feat(auth) : Updated Login API & cleaned up the code

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,20 +8,4 @@ const adminAuth = (req, res, next) => {
   }
 };
 
-const userAuth = (req, res, next) => {
-  let token = req.body.token;
-  console.log(req.body.token);
-  console.log(token);
-  try {
-    let isUserAuthorized = token === "abc";
-    if (!isUserAuthorized) {
-      throw new Error("User is not authorized.");
-    } else {
-      next();
-    }
-  } catch (err) {
-    res.status(401).send(err.message);
-  }
-};
-
-module.exports = { adminAuth, userAuth };
+module.exports = { adminAuth };
